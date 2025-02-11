26b2cc712d9ef33bf0156123e1fed9c531fb62c8
Olawale7991 <olawalelokoso000@gmail.com>
2025-02-11 23:58:58 +0100

    checking

---
+++ js/landing.js
@@ -0,0 +1,21 @@
+document.getElementById('menuToggle').addEventListener('click', function() {
+    var container = document.querySelector('.menu-container');
+    var hamburgerIcon = document.querySelector('.menu-toggle .hambuger'); 
+    var cancelIcon = document.querySelector('.menu-toggle .cancel');
+  
+    if (!container || !hamburgerIcon || !cancelIcon) {
+      console.error("One or more elements were not found:", { container, hamburgerIcon, cancelIcon });
+      return;
+    }
+  
+    container.classList.toggle('menu-visible');
+  
+    if (container.classList.contains('menu-visible')) {
+      hamburgerIcon.style.display = 'none';
+      cancelIcon.style.display = 'block';
+    } else {
+      hamburgerIcon.style.display = 'block';
+      cancelIcon.style.display = 'none';
+    }
+  });
+  
